7b702f8095e568de8d41ebcf3250ae3bab0233ab
HANSON BI TRAN <hansonb.tran@HANSONs-MacBook-Air.local>
2024-04-26 16:25:50 -0700

    updated TreeNode class

--- src/trees/FamilyTree.java
+++ src/trees/FamilyTree.java
@@ -5,6 +5,8 @@ import java.io.*;
 import javax.swing.*;
 import javax.swing.filechooser.*;
 
+import FamilyTree.TreeNode;
+
 
 public class FamilyTree
 {
@@ -31,8 +33,8 @@ public class FamilyTree
         
         void addChild(TreeNode childNode)
         {
-            // Add childNode to this node's children list. Also
-            // set childNode's parent to this node.
+        	 childNode.parent = this; 
+             children.add(childNode);
         }
         
         
@@ -41,14 +43,15 @@ public class FamilyTree
         TreeNode getNodeWithName(String targetName)
         {
             // Does this node have the target name?
-            if (?????)
+            if (this.name.equals(targetName))
                 return this;
                     
             // No, recurse. Check all children of this node.
             for (TreeNode child: children)
             {
-                // If child.getNodeWithName(targetName) returns a non-null node,
-                // then that's the node we're looking for. Return it.
+            	TreeNode foundNode = child.getNodeWithName(targetName);
+                if (foundNode != null)
+                    return foundNode;
             }
             
             // Not found anywhere.
@@ -62,10 +65,11 @@ public class FamilyTree
         {
             ArrayList<TreeNode> ancestors = new ArrayList<>();
 
-            // ?????  Collect ancestors of this TreeNode into the array list. HINT: going up
-            // the nodes of a tree is like traversing a linked list. If that isn’t clear,
-            // draw a tree, mark any leaf node, and then mark its ancestors in order from
-            // recent to ancient. Expect a question about this on the final exam.
+            TreeNode curNode = this;
+            while (curNode.parent != null) {
+            	ancestors.add(curNode.parent);
+            	curNode = curNode.parent;
+            }
 
             return ancestors;
         }
